52e7a4288e30781a00bd10a510103ca1e5222399
fml37 <felipelai@gmail.com>
2020-11-11 01:06:06 -0500

    Prototype creation (#8)
    * Prototyping 0.1
    
    Added Movement
    Bullet Spawning
    Template for object kill box
    
    Co-authored-by: fml37 <fml37@drexel.edu>

---
+++ Assets/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody2D m_Rigidbody;
+    public float b_speed;
+    public float max_Velocity;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Rigidbody.AddForce(transform.up * b_speed);
+    }
+}
---
+++ Assets/Control.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    Rigidbody2D m_Rigidbody;
+    public float m_Speed;
+    public float turn_Speed;
+    public GameObject myPrefab;
+
+
+    void Start()
+    {
+        //Fetch the Rigidbody component you attach from your GameObject
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(myPrefab, transform.position, transform.rotation);
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //Apply force to the "up" of the object, moving object forward
+            m_Rigidbody.AddForce (transform.up * m_Speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //Apply negative force to the "up" of the object, moving object backwards
+            m_Rigidbody.AddForce(-transform.up * m_Speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Rotate the sprite about the Y axis in the positive direction
+            transform.Rotate(new Vector3(0, 0, -1) * Time.deltaTime * turn_Speed, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Rotate the sprite about the Y axis in the negative direction
+            transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime * turn_Speed, Space.World);
+        }
+    }
+}
